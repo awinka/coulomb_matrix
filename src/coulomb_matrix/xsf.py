4bd86843db1ae5ca072a5b70f0fc86f782a9132d
awinka <awinka@iis.ee.ethz.ch>
2026-07-10 11:18:25 +0200

    Include skip_wf parameter for (hopefully) faster reading of lattice data

--- src/coulomb_matrix/xsf.py
+++ src/coulomb_matrix/xsf.py
@@ -6,9 +6,28 @@ Simple XSF reader adapted from existing xsf_2_np.py
 
 
 class Xsf2Np:
-    def __init__(self, file_obj):
+    def __init__(self, file_obj, skip_wf=False):
+        """Initialize the Xsf2Np object.
+
+        Parameters
+        ----------
+        file_obj : file-like object
+            A file-like object containing the XSF data.
+        skip_wf : bool, optional
+            If True, skip reading the Wannier function data. This enables faster reading when only lattice information is needed. Default is False.
+        """
         self.file_obj = file_obj
-        self.lattice_vectors, self.atoms, self.nx, self.ny, self.nz, self.WF, self.origin, self.supercell = self.read_xsf()
+        self.skip_wf = skip_wf
+        (
+            self.lattice_vectors,
+            self.atoms,
+            self.nx,
+            self.ny,
+            self.nz,
+            self.WF,
+            self.origin,
+            self.supercell,
+        ) = self.read_xsf()
 
     def read_xsf(self):
         for line in self.file_obj:
@@ -34,7 +53,6 @@ class Xsf2Np:
                 line = self.file_obj.readline()
                 nx, ny, nz = line.split()
                 nx, ny, nz = int(nx), int(ny), int(nz)
-                WF = np.zeros((nx, ny, nz))
                 line = self.file_obj.readline()
                 origin = np.array([float(x) for x in line.split()])
                 supercell = np.zeros((3, 3))
@@ -42,15 +60,18 @@ class Xsf2Np:
                 for i in range(3):
                     line = self.file_obj.readline()
                     supercell[i] = np.array([float(x) for x in line.split()])
-                for iz in range(nz):
-                    for iy in range(ny):
-                        for ix in range(nx):
-                            tr = iz * ny * nx + iy * nx + ix
-                            if not tr % 6:
-                                itr += 1
-                                line = self.file_obj.readline()
-                                values = [float(x) for x in line.split()]
-                            WF[ix, iy, iz] = values[tr % 6]
+                WF = None
+                if not self.skip_wf:
+                    WF = np.zeros((nx, ny, nz))
+                    for iz in range(nz):
+                        for iy in range(ny):
+                            for ix in range(nx):
+                                tr = iz * ny * nx + iy * nx + ix
+                                if not tr % 6:
+                                    itr += 1
+                                    line = self.file_obj.readline()
+                                    values = [float(x) for x in line.split()]
+                                WF[ix, iy, iz] = values[tr % 6]
             elif line == "END_DATAGRID_3D":
                 return lattice_vectors, atoms, nx, ny, nz, WF, origin, supercell
 
@@ -74,12 +95,12 @@ class Xsf2Np:
 
     def linereader(self, line):
         line = line.strip()
-        if not line or line.startswith('#'):
+        if not line or line.startswith("#"):
             self.linereader(self.file_obj.readline())
         return line
 
 
 if __name__ == "__main__":
-    path = '/usr/scratch/bucaramanga/awinka/MoS2/proj/wannier90_00001.xsf'
-    with open(path, 'r') as file:
+    path = "/usr/scratch/bucaramanga/awinka/MoS2/proj/wannier90_00001.xsf"
+    with open(path, "r") as file:
         xsf = Xsf2Np(file)
